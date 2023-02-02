365ceb5b1cee16fb6be3c09af155c64989b3ada0
chngliann <chngliann11@gmail.com>
2023-02-02 21:18:19 +0000

    setup

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+//Q: why downloaded bootstrap through reactstrap?? also why he skip some seemingly core command runs??
+//before the imports:
+// install Font Awesome React components: npm i --save @fortawesome/react-fontawesome@latest
+// import bootstrap in app code: npm install --save bootstrap
+
+// import React components
+import React, { useState } from "react"; //useState Hook allows us to track state(data, properties,..) in a function component
+
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+
+// import fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faPen,
+  faTrash, // mark task as completed, edit task, delete task
+} from "@fortawesome/free-solid-svg-icons";
+
+import "./App.css";
 
 function App() {
+  const [toDo, setToDo] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //use bootstrap's container class?
+    <div className="container App">
+      <h2>To Do List (ReactJS)</h2>
     </div>
   );
 }
